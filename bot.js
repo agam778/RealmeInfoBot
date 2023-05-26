d2a7c01101e8a1900f07d4c1b1f154d193f1f5cb
Agampreet Singh <68941022+agam778@users.noreply.github.com>
2023-05-26 16:10:33 +0000

    refactor: Improve code structure and error handling
    - Refactor the code to improve its structure and readability.
    - Enhance error handling by adding try-catch blocks and throwing explicit errors for missing environment variables.
    - Move the command logging logic to a separate function for modularity and reusability.
    - Use path.resolve instead of path.join for obtaining absolute paths.
    - Gracefully handle errors during bot startup.

--- bot.js
+++ bot.js
@@ -7,85 +7,90 @@ if (fs.existsSync('.env')) {
   require('dotenv').config()
 }
 
-if (!process.env.LOG_CHANNEL) {
-  console.error('LOG_CHANNEL not set in environment variables! Exiting...')
-  process.exit(1)
-}
-
-if (!process.env.BOT_TOKEN) {
-  console.error('BOT_TOKEN not set in environment variables! Exiting...')
-  process.exit(1)
-}
-
 const logchannel = process.env.LOG_CHANNEL
+if (!logchannel) {
+  throw new Error('LOG_CHANNEL not set in environment variables! Exiting...')
+}
 
-const bot = new Bot(process.env.BOT_TOKEN)
-bot.use(autoQuote)
+const botToken = process.env.BOT_TOKEN
+if (!botToken) {
+  throw new Error('BOT_TOKEN not set in environment variables! Exiting...')
+}
 
-const commandFilesDir = path.join(__dirname, 'commands')
-const commandFiles = fs
-  .readdirSync(commandFilesDir)
-  .filter((file) => file.endsWith('.js'))
-for (const file of commandFiles) {
-  const command = require(path.join(commandFilesDir, file))
-  bot.command(command.name, command.handler)
-  if (command.alias) {
-    for (const alias of command.alias) {
-      bot.command(alias, command.handler)
+async function start() {
+  const bot = new Bot(botToken)
+  bot.use(autoQuote)
+
+  const commandFilesDir = path.resolve(__dirname, 'commands')
+  const commandFiles = fs
+    .readdirSync(commandFilesDir)
+    .filter((file) => file.endsWith('.js'))
+  for (const file of commandFiles) {
+    const command = require(path.join(commandFilesDir, file))
+    bot.command(command.name, command.handler)
+    if (command.alias) {
+      for (const alias of command.alias) {
+        bot.command(alias, command.handler)
+      }
     }
   }
+
+  bot.command('start', (ctx) =>
+    ctx.reply(
+      'Hello! I am RealmeInfoBot, and I can tell you the name of the model if you prove me the codename, vice versa.\n\n' +
+        'Run the /help command to see the list of commands!'
+    )
+  )
+
+  bot.catch((err) => {
+    const ctx = err.ctx
+    console.log(`Error while handling update ${ctx.update.update_id}:`)
+    const e = err.error
+    const commandText = ctx.message.text
+    const chatTitle = ctx.chat.title
+    const errorMessage = `Command: <code>${commandText}</code>\n\nUser: ${ctx.from.first_name} ${ctx.from.last_name} (${ctx.from.username})\n\nGroup: <code>${chatTitle}</code>\n\nError: <code>${e}</code>`
+
+    if (e instanceof GrammyError) {
+      ctx.api.sendMessage(logchannel, `Error in request:\n\n${errorMessage}`, {
+        parse_mode: 'HTML',
+        chat_id: logchannel,
+      })
+      ctx.reply('Error in request:', e.description)
+    } else if (e instanceof HttpError) {
+      ctx.api.sendMessage(
+        logchannel,
+        `Could not contact Telegram:\n\n${errorMessage}`,
+        { parse_mode: 'HTML', chat_id: logchannel }
+      )
+      ctx.reply('Could not contact Telegram:', e)
+    } else {
+      ctx.reply(`Oops, an error occurred!\n\n${errorMessage}`, {
+        parse_mode: 'HTML',
+        chat_id: logchannel,
+      })
+      ctx.reply('Oops, an error occurred!\n\n' + e)
+    }
+  })
+
+  process.on('uncaughtException', (err) => {
+    console.error(err)
+  })
+
+  process.on('unhandledRejection', (err) => {
+    console.error(err)
+  })
+
+  process.on('SIGINT', () => {
+    console.log('Stopping @RealmeInfoBot...')
+    bot.stop()
+    process.exit(0)
+  })
+
+  console.log('Starting @RealmeInfoBot...')
+  await bot.start()
 }
 
-bot.command('start', (ctx) =>
-  ctx.reply(
-    'Hello! I am RealmeInfoBot, and I can tell you the name of the model if you prove me the codename, vice versa.\n\n' +
-      'Run the /help command to see the list of commands!'
-  )
-)
-
-bot.catch((err) => {
-  const ctx = err.ctx
-  console.log(`Error while handling update ${ctx.update.update_id}:`)
-  const e = err.error
-  const commandText = ctx.message.text
-  const chatTitle = ctx.chat.title
-  const errorMessage = `Command: <code>${commandText}</code>\n\nUser: ${ctx.from.first_name} ${ctx.from.last_name} (${ctx.from.username})\n\nGroup: <code>${chatTitle}</code>\n\nError: <code>${e}</code>`
-
-  if (e instanceof GrammyError) {
-    ctx.api.sendMessage(logchannel, `Error in request:\n\n${errorMessage}`, {
-      parse_mode: 'HTML',
-      chat_id: logchannel,
-    })
-    ctx.reply('Error in request:', e.description)
-  } else if (e instanceof HttpError) {
-    ctx.api.sendMessage(
-      logchannel,
-      `Could not contact Telegram:\n\n${errorMessage}`,
-      { parse_mode: 'HTML', chat_id: logchannel }
-    )
-    ctx.reply('Could not contact Telegram:', e)
-  } else {
-    ctx.reply(`Oops, an error occurred!\n\n${errorMessage}`, {
-      parse_mode: 'HTML',
-      chat_id: logchannel,
-    })
-    ctx.reply('Oops, an error occurred!\n\n' + e)
-  }
+start().catch((error) => {
+  console.error('Error occurred during bot startup:', error)
+  process.exit(1)
 })
-
-process.on('uncaughtException', (err) => {
-  console.error(err)
-})
-
-process.on('unhandledRejection', (err) => {
-  console.error(err)
-})
-
-process.on('SIGINT', () => {
-  console.log('Stopping @RealmeInfoBot...')
-  bot.stop()
-  process.exit(0)
-})
-
-console.log('Starting @RealmeInfoBot...')
-bot.start()
