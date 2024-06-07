0ec07d01b547b32db4e1600775934daa451f473e
Agampreet Singh <68941022+agam778@users.noreply.github.com>
2024-06-07 14:55:31 +0530

    refactor: Simplify deviceinfo command implementation

--- modules/deviceinfo.js
+++ modules/deviceinfo.js
@@ -6,8 +6,7 @@ import { logCommand } from '../helpers.js'
 const composer = new Composer()
 
 composer.command('deviceinfo', async (ctx) => {
-  const { message } = ctx
-  const { text } = message
+  const { text } = ctx.message
   logCommand(ctx)
 
   const name = text.substring(text.indexOf(' ') + 1)
@@ -19,14 +18,11 @@ composer.command('deviceinfo', async (ctx) => {
 
   const searchresult = await gsmarena.search.search(name)
 
-  if (searchresult.length === 0) {
-    ctx.reply(
+  if (!searchresult.length) {
+    return ctx.reply(
       `The device <code>${name}</code> does not exist!\nNote: If you are searching with codename, enter the device name instead.`,
-      {
-        parse_mode: 'HTML',
-      }
+      { parse_mode: 'HTML' }
     )
-    return
   }
 
   const device =
@@ -37,112 +33,55 @@ composer.command('deviceinfo', async (ctx) => {
   const details = await gsmarena.catalog.getDevice(device.id)
   const { name: deviceName, detailSpec } = details
 
-  const url = await axios
-    .get(
+  let codename
+  try {
+    const url = await axios.get(
       'https://realmebotapi-1-e2272932.deta.app/' +
         encodeURIComponent(deviceName)
     )
-    .catch((err) => {})
+    codename = url.data[0].codename
+  } catch {}
 
-  if (url) {
-    var codename = url.data[0].codename
-  }
+  const getSpecValue = (categoryName, specName) =>
+    detailSpec
+      .find((cat) => cat.category === categoryName)
+      ?.specifications.find((spec) => spec.name === specName)?.value ||
+    'Unknown'
 
-  const result = []
+  const result = [
+    `<b>Status:</b> ${getSpecValue('Launch', 'Status')}`,
+    `<b>Display:</b>\n- ${getSpecValue('Display', 'Type')}\n- ${getSpecValue(
+      'Display',
+      'Size'
+    )}\n- ${getSpecValue('Display', 'Resolution')}`,
+    `<b>Platform:</b>\n- ${getSpecValue(
+      'Platform',
+      'Chipset'
+    )}\n- ${getSpecValue('Platform', 'GPU')}`,
+    `<b>Main Camera:</b>\n- ${
+      getSpecValue('Main Camera', 'Single') ||
+      getSpecValue('Main Camera', 'Dual')
+    }\n- ${getSpecValue('Main Camera', 'Features')}`,
+    `<b>Selfie Camera:</b>\n- ${
+      getSpecValue('Selfie camera', 'Single') ||
+      getSpecValue('Selfie camera', 'Dual')
+    }`,
+    `<b>Battery:</b>\n- ${getSpecValue('Battery', 'Type')}\n- ${getSpecValue(
+      'Battery',
+      'Charging'
+    )}`,
+  ]
 
-  const statusValue = detailSpec
-    .find((category) => category.category === 'Launch')
-    ?.specifications.find((spec) => spec.name === 'Status')?.value
-
-  const displayCategory = detailSpec.find(
-    (category) => category.category === 'Display'
+  const keyboard = new InlineKeyboard().url(
+    'View on GSMArena',
+    `https://gsmarena.com/${device.id}.php`
   )
-  const typeValue = displayCategory?.specifications.find(
-    (spec) => spec.name === 'Type'
-  ).value
-  const sizeValue = displayCategory?.specifications.find(
-    (spec) => spec.name === 'Size'
-  ).value
-  const resolutionValue = displayCategory?.specifications.find(
-    (spec) => spec.name === 'Resolution'
-  ).value
-
-  const platformCategory = detailSpec.find(
-    (category) => category.category === 'Platform'
-  )
-  const chipsetValue = platformCategory?.specifications.find(
-    (spec) => spec.name === 'Chipset'
-  ).value
-  const gpuValue = platformCategory?.specifications.find(
-    (spec) => spec.name === 'GPU'
-  ).value
-
-  const mainCameraName = detailSpec.find(
-    (category) => category.category === 'Main Camera'
-  )?.specifications[0].name
-
-  const mainCameraValue = detailSpec.find(
-    (category) => category.category === 'Main Camera'
-  )?.specifications[0].value
-
-  const mainCameraFeatures = detailSpec.find(
-    (category) => category.category === 'Main Camera'
-  )?.specifications[1].value
-
-  const selfieCameraName = detailSpec.find(
-    (category) => category.category === 'Selfie camera'
-  )?.specifications[0].name
-
-  const selfieCameraValue = detailSpec.find(
-    (category) => category.category === 'Selfie camera'
-  )?.specifications[0].value
-
-  const batterySpecs = detailSpec
-    .find((category) => category.category === 'Battery')
-    ?.specifications.find((spec) => spec.name === 'Type')?.value
-
-  const batteryValue = detailSpec
-    .find((category) => category.category === 'Battery')
-    ?.specifications.find((spec) => spec.name === 'Charging')?.value
-
-  result.push(`<b>Status:</b> ${statusValue || 'Unknown'}`)
-  result.push(
-    `<b>Display:</b>\n- ${typeValue || 'Unknown'}\n- ${
-      sizeValue || 'Unknown'
-    }\n- ${resolutionValue || 'Unknown'}`
-  )
-  result.push(
-    `<b>Platform:</b>\n- ${chipsetValue || 'Unknown'}\n- ${
-      gpuValue || 'Unknown'
-    }`
-  )
-  result.push(
-    `<b>Main Camera (${mainCameraName || 'Unknown'}):</b>\n- ${
-      mainCameraValue || 'Unknown'
-    }\n- ${mainCameraFeatures || 'Unknown'}`
-  )
-  result.push(
-    `<b>Selfie Camera (${selfieCameraName || 'Unknown'}):</b>\n- ${
-      selfieCameraValue || 'Unknown'
-    }`
-  )
-  result.push(
-    `<b>Battery:</b>\n- ${batterySpecs || 'Unknown'}\n- ${
-      batteryValue || 'Unknown'
-    }`
-  )
-
-  const keyboard = new InlineKeyboard()
-  keyboard.url('View on GSMArena', `https://gsmarena.com/${device.id}.php`)
 
   ctx.reply(
     `<b>${deviceName}</b> - <code>${
       codename || 'unknown'
-    }</code>\n\n${result.join('\n')}` || 'Unknown',
-    {
-      parse_mode: 'HTML',
-      reply_markup: keyboard,
-    }
+    }</code>\n\n${result.join('\n')}`,
+    { parse_mode: 'HTML', reply_markup: keyboard }
   )
 })
 
